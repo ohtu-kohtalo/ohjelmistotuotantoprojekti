36f6afe3363e2d0e3b1f3951a905e2bb2fda1e93
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2025-01-24 12:21:56 +0200

    Fix formatting app.py

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api/gemini')
+
+@app.route("/api/gemini")
 def query():
     return "Hello, World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
