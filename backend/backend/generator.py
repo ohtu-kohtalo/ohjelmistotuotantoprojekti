29f01d4b9d09dc72fe72cec886a79963834380f9
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2025-01-29 10:32:51 +0200

    Add optional website parameter for creating agents

--- backend/backend/generator.py
+++ backend/backend/generator.py
@@ -7,14 +7,20 @@ class Generator:
     def __init__(self, llm=Gemini()):
         self.__llm = llm
 
-    def create_agents(self, company: str, industry: str, number_of_agents: str) -> str:
+    def create_agents(
+        self,
+        company: str,
+        industry: str,
+        number_of_agents: str,
+        website_data: str = None,
+    ) -> str:
         """Creates agents based on the given parameters.
 
         Arguments:
         ----------
             company: the name of the company.
             industry: the industry where the company operates.
-            web_site: the URL for the website of the company
+            website_data: (Optional) Crawled data from the the website of the company.
             number_of_agents: the number of agents this method will create.
 
         Returns:
@@ -24,5 +30,7 @@ class Generator:
             f"Simulate {number_of_agents} customer profiles for a company. The name "
             f"of the company is {company} and its industry is {industry}"
         )
+        if website_data:
+            prompt += f". Website data for the company: {website_data}"
         response = self.__llm.get_response(prompt)
         return response
