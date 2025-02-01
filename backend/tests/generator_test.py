caf63ae140278a4da46c9f4634271971faf11c5e
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-02-01 23:28:46 +0200

    fix formatting

--- backend/tests/generator_test.py
+++ backend/tests/generator_test.py
@@ -9,16 +9,18 @@ class TestGenerator(unittest.TestCase):
         self.mock_gemini = MagicMock()
         self.mock_gemini.get_response.side_effect = lambda x: x
         self.generator = Generator(self.mock_gemini)
-    
-    @patch('pandas.read_csv') 
+
+    @patch("pandas.read_csv")
     def test_simulate_customer(self, mock_read_csv):
-        mock_data = pd.DataFrame({
-            'T1': [1, 2, 3],
-            'T2': [1, 2, 3],
-            'T3': [1, 2, 3],
-            'T4': [1, 2, 3],
-            'T5': [1, 2, 3],
-        })
+        mock_data = pd.DataFrame(
+            {
+                "T1": [1, 2, 3],
+                "T2": [1, 2, 3],
+                "T3": [1, 2, 3],
+                "T4": [1, 2, 3],
+                "T5": [1, 2, 3],
+            }
+        )
         mock_read_csv.return_value = mock_data
         response = self.generator.create_agents("Mokia", "IT", 3)
         should_be = (
