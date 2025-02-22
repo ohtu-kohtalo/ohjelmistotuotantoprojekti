56b0136ca8142bfad0534d7a40f3ab1147fd5ff8
tuome <tonni@tuta.io>
2025-02-22 19:14:34 +0200

    Format with black

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -43,9 +43,11 @@ def create_agent():
     response = jsonify(response)
     return response
 
+
 @app.route("/health", methods=["GET"])
 def health_check():
     return "OK", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5500, debug=True)
