2a3f3590758c05d329e3b4dee2bd94107755e2dc
tuome <tonni@tuta.io>
2025-03-06 09:46:08 +0200

    Remove InitialDistribution title

--- frontend/src/pages/InitialDistribution.jsx
+++ frontend/src/pages/InitialDistribution.jsx
@@ -4,7 +4,6 @@ import PlotContainer from "../components/PlotContainer";
 
 const InitialDistribution = ({ distributions }) => (
   <div className="card">
-    <Title className="card-header" text={"Agent Pool Distribution Graphs"} />
     <div className="graph-placeholder">
       <PlotContainer agentData={distributions} />
     </div>
