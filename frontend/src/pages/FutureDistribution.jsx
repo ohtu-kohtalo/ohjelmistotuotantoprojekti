46fb468dfe8a2809941e3b2dceae0559d9463f53
tuome <tonni@tuta.io>
2025-03-18 23:43:37 +0200

    Add csv download button to Future Distribution tab

--- frontend/src/pages/FutureDistribution.jsx
+++ frontend/src/pages/FutureDistribution.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import CsvDownload from "../components/CsvDownload";
 
-const FutureDistribution = () => {
+const FutureDistribution = ({ csvUploaded }) => {
   return (
     <div id="FutureDistribution" className="card active">
       <h3 className="card-header">Future Answer Distribution Graphs || TODO</h3>
-      <div className="graph-placeholder">
+      {/* <div className="graph-placeholder">
         <div className="variable-buttons">
           <button className="variable-button">Age</button>
           <button className="variable-button">Gender</button>
@@ -12,7 +13,8 @@ const FutureDistribution = () => {
         <div className="plot-area">
           <p>[Graph Placeholder]</p>
         </div>
-      </div>
+      </div> */}
+      {csvUploaded && <CsvDownload />}
     </div>
   );
 };
