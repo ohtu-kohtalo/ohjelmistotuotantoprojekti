2f2b3e4927c17926ae346a11fac73cfeb72bb4c0
tuome <tonni@tuta.io>
2025-03-19 00:38:48 +0200

    Remove csv download button from Future Distribution

--- frontend/src/pages/FutureDistribution.jsx
+++ frontend/src/pages/FutureDistribution.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CsvDownload from "../components/CsvDownload";
 
-const FutureDistribution = ({ csvUploaded }) => {
+const FutureDistribution = () => {
   return (
     <div id="FutureDistribution" className="card active">
       <h3 className="card-header">Future Answer Distribution Graphs || TODO</h3>
-      {/* <div className="graph-placeholder">
+      <div className="graph-placeholder">
         <div className="variable-buttons">
           <button className="variable-button">Age</button>
           <button className="variable-button">Gender</button>
@@ -13,8 +13,7 @@ const FutureDistribution = ({ csvUploaded }) => {
         <div className="plot-area">
           <p>[Graph Placeholder]</p>
         </div>
-      </div> */}
-      {csvUploaded && <CsvDownload />}
+      </div>
     </div>
   );
 };
