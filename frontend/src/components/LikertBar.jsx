bc1172f604e33567946773459fb452818af7f826
sonjaolkkonen <sonjaolkkonen@users.noreply.github.com>
2025-03-23 14:31:22 +0000

    Prettified Code!

--- frontend/src/components/LikertBar.jsx
+++ frontend/src/components/LikertBar.jsx
@@ -26,7 +26,13 @@ const LikertBar = ({ data, question }) => {
 
     const colorScale = d3
       .scaleOrdinal()
-      .domain(["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"])
+      .domain([
+        "Strongly Disagree",
+        "Disagree",
+        "Neutral",
+        "Agree",
+        "Strongly Agree",
+      ])
       .range(["#FF0000", "#FFA500", "#FFFF00", "#00FF00", "#006400"]);
 
     const svgContainer = svg.attr("width", width).attr("height", height);
