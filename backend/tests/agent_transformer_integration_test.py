2866e2f1159ea71ceb9b2acf4e3f56cff59c9740
Jaakko Airikkala <jaakkel@gmail.com>
2025-05-07 16:24:48 +0300

    Add tests for AgentTransformer

--- backend/tests/agent_transformer_integration_test.py
+++ backend/tests/agent_transformer_integration_test.py
@@ -1,7 +1,7 @@
 import unittest
+from unittest.mock import Mock
 from backend.services.agent_transformer import AgentTransformer
 from backend.entities.agent import Agent
-from unittest.mock import Mock
 
 
 class TestAgentTranformerIntegration(unittest.TestCase):
@@ -11,8 +11,10 @@ class TestAgentTranformerIntegration(unittest.TestCase):
 
     def setUp(self):
         """Set up sample agents for testing."""
+        self.scenario = "This is a future scenario"
+        self.mock_llm = Mock()
 
-        def latent_variables(age: str, gender: str, latent_values: list):
+        def agent_variables(age: str, gender: str, latent_values: list):
             """Returns an info dictionary that can be used to create Agent instances"""
             info = {
                 "Age": age,
@@ -26,15 +28,14 @@ class TestAgentTranformerIntegration(unittest.TestCase):
 
         # Create a list of agents
         self.agents = [
-            Agent(latent_variables("19", "male", [2.3, -0.1])),
-            Agent(latent_variables("19", "male", [-3.0, -0.2])),
-            Agent(latent_variables("19", "male", [0.0, -0.4])),
-            Agent(latent_variables("19", "male", [1.9, 0.0])),
+            Agent(agent_variables("19", "male", [2.3, -0.1])),
+            Agent(agent_variables("20", "male", [-3.0, -0.2])),
+            Agent(agent_variables("19", "female", [0.0, -0.4])),
+            Agent(agent_variables("21", "female", [1.9, 0.0])),
         ]
 
     def test_transform_agents_to_future(self):
         """Tests that transform_agents_to_future works correctly with good inputs."""
-        mock_llm = Mock()
 
         # Set the response llm will give
         llm_answer = """
@@ -55,9 +56,97 @@ Respondent 4:
 1.3
 0.0
 """
-        mock_llm.get_response.return_value = llm_answer
+        self.mock_llm.get_response.return_value = llm_answer
 
-        transformer = AgentTransformer(mock_llm)
-        scenario = "This is a future scenario"
-        succeeded = transformer.transform_agents_to_future(self.agents, scenario)
+        transformer = AgentTransformer(self.mock_llm)
+        succeeded = transformer.transform_agents_to_future(self.agents, self.scenario)
         self.assertTrue(succeeded)
+
+    def test_too_few_respondents(self):
+        """Tests that transform_agents_to_future raises an error if all respondents did not get new
+        variables."""
+
+        # Set too few respondents in the llm's response
+        llm_answer = """
+
+Respondent 1:
+0.1
+-0.2
+
+Respondent 2:
+1.3
+2.3
+
+Respondent 3:
+0.0
+-2.2
+
+"""
+        self.mock_llm.get_response.return_value = llm_answer
+        transformer = AgentTransformer(self.mock_llm)
+
+        with self.assertRaises(RuntimeError):
+            transformer.transform_agents_to_future(self.agents, self.scenario)
+
+    def test_too_many_respondents(self):
+        """Tests that transform_agents_to_future raises an error if the llm's response has too many
+        respondents."""
+
+        # Set too many respondents in the llm's response
+        llm_answer = """
+
+Respondent 1:
+0.1
+-0.2
+
+Respondent 2:
+1.3
+2.3
+
+Respondent 3:
+0.0
+-2.2
+
+Respondent 4:
+0.0
+-3.2
+
+Respondent 5:
+0.0
+-1.2
+
+"""
+        self.mock_llm.get_response.return_value = llm_answer
+        transformer = AgentTransformer(self.mock_llm)
+
+        with self.assertRaises(RuntimeError):
+            transformer.transform_agents_to_future(self.agents, self.scenario)
+
+    def test_too_few_variables(self):
+        """Tests that transform_agents_to_future raises an error if the llm's response has too few
+        new variables for some respondent."""
+
+        # Set too many respondents in the llm's response
+        llm_answer = """
+
+Respondent 1:
+0.1
+-0.2
+
+Respondent 2:
+1.3
+2.3
+
+Respondent 3:
+0.2
+
+Respondent 4:
+0.0
+-3.2
+
+"""
+        self.mock_llm.get_response.return_value = llm_answer
+        transformer = AgentTransformer(self.mock_llm)
+
+        with self.assertRaises(RuntimeError):
+            transformer.transform_agents_to_future(self.agents, self.scenario)
